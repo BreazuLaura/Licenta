a21875cd52fb03ce25e57b7fae5c102c74b90ce2
LauraBreazu <laura14breazu@gmail.com>
2024-06-02 23:00:31 +0300

    add login frontend and fix page allignment

--- frontend/licenta-app/src/app/app-routing.module.ts
+++ frontend/licenta-app/src/app/app-routing.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'view-marketplace', component: ProductListComponent },
-  { path: 'user-form', component: ProductFormComponent },
+  { path: 'product-form', component: ProductFormComponent },
+  { path: 'user-form', component: UserFormComponent },
+  { path: 'user-form/:id', component: UserFormComponent },
+  { path: 'user-details/:id', component: UserDetailsComponent },
+  { path: 'users', component: UserListComponent },
   { path: 'ecommerce/:id', component: ProductDetailComponent },
 ];
 
--- frontend/licenta-app/src/app/app.module.ts
+++ frontend/licenta-app/src/app/app.module.ts
@@ -2,31 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
-
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'view-marketplace', component: ProductListComponent },
-  { path: 'user-form', component: ProductFormComponent },
-  { path: 'user-form/:id', component: UserFormComponent },
-  { path: 'user-details/:id', component: UserDetailsComponent },
-  { path: 'users', component: UserListComponent },
-  { path: 'ecommerce/:id', component: ProductDetailComponent },
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent,
     ProductListComponent,
     ProductFormComponent,
     ProductDetailComponent,
@@ -38,7 +31,7 @@ const routes: Routes = [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
