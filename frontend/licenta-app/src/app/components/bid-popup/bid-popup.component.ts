e4ab6aa3896e339ef2271ba5448ceb241ec4702c
LauraBreazu <laura14breazu@gmail.com>
2024-06-06 21:41:36 +0300

    greater bid condition

--- frontend/licenta-app/src/app/components/bid-popup/bid-popup.component.ts
+++ frontend/licenta-app/src/app/components/bid-popup/bid-popup.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-
+import { Auction } from '../../models/auction'; // Adjust the path as necessary
+import { MatSnackBar } from '@angular/material/snack-bar'; // Add this import for notifications
 
 @Component({
   selector: 'app-bid-popup',
@@ -8,18 +9,29 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./bid-popup.component.css']
 })
 export class BidPopupComponent {
-  bidAmount: number | undefined;
+  bidAmount?: number;
 
   constructor(
     public dialogRef: MatDialogRef<BidPopupComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) { }
+    @Inject(MAT_DIALOG_DATA) public data: { auction: Auction },
+    private snackBar: MatSnackBar // Inject MatSnackBar
+  ) {}
 
   onCancel(): void {
     this.dialogRef.close();
   }
 
   onSaveBid(): void {
-    this.dialogRef.close(this.bidAmount);
+    if (this.bidAmount !== undefined) {
+      const currentHighestBid = this.data.auction.currentHighestBid ? this.data.auction.currentHighestBid.amount : 0;
+      // @ts-ignore
+      if (this.bidAmount > currentHighestBid) {
+        this.dialogRef.close(this.bidAmount);
+      } else {
+        this.snackBar.open(`The amount of bid must be greater than ${currentHighestBid}`, 'Close', {
+           duration: 5000
+        });
+      }
+    }
   }
 }
